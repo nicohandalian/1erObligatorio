ed0c1979c91ba63dd8f3f4c63119b9f9af19dda8
Nicolas <nicolas.handalian@gmail.com>
2019-05-01 01:32:30 -0300

    alert when checkout

--- 1erObligatorio/Controllers/CheckoutViewController.swift
+++ 1erObligatorio/Controllers/CheckoutViewController.swift
@@ -19,10 +19,24 @@ class CheckoutViewController: UIViewController {
         totalPriceLabel.text = "$" + String(trolley.getTotalPrice())
     }
     
+    func indexHandler(alert: UIAlertAction!){
+        let indexNavigationController = storyboard?.instantiateViewController(withIdentifier: "IndexViewController") as! IndexViewController
+        
+        present(indexNavigationController, animated: true, completion: nil)
+        
+    }
+    
     @IBAction func checkoutButtonPressed(_ sender: Any) {
         trolley.clear()
-        let indexNavigationController = storyboard?.instantiateViewController(withIdentifier: "IndexViewController") as! IndexViewController
-        present(indexNavigationController, animated: true, completion: nil)
+        
+        let alert = UIAlertController(title: "Successful checkout", message: "Your purchase has been made successfully.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: indexHandler))
+        
+        self.present(alert, animated: true)
+        
+        
     }
+    
     
 }
