eec6b42a81de63a59a7b9d65313dea35ab9c0813
Nicolas <nicolas.handalian@gmail.com>
2019-05-29 03:04:41 -0300

    checkoutcell height changed

--- 1erObligatorio/Controllers/CheckoutViewController.swift
+++ 1erObligatorio/Controllers/CheckoutViewController.swift
@@ -160,7 +160,7 @@ extension CheckoutViewController: UICollectionViewDataSource, UICollectionViewDe
 
 extension CheckoutViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: (collectionView.frame.width/2) - 7.5, height: (collectionView.frame.height/1.5))
+        return CGSize(width: (collectionView.frame.width/2) - 7.5, height: (collectionView.frame.height/1.3))
     }
 }
 
